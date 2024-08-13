4bbd32bdd803f066dfe54752395b769f6b8ede01
MSI-NoteBook\Richie <targetactionpersist@gmail.com>
2024-08-13 22:38:40 +0800

    測試用專案
    測試用專案

---
+++ WebMVCTestEx_NoHttps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebMVCTestEx_NoHttps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
